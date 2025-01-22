181e8c00aaf9ddbe24f28c906c02d4cad7b249fc
renatohvo <renatohvo@gmail.com>
2025-01-21 22:44:12 -0300

    feat: :sparkles: função soma adicionada

---
+++ math_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := Soma(20, 20)
+
+	if total != 40 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 40)
+	}
+}
